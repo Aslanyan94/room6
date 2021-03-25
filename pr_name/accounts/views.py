fa0231d1b7cb67e4d6db8cc3368a84fc70958e3a
aslanyan94 <aslanyan.p.aro@gmail.com>
2021-03-25 19:19:53 +0400

    save fields in DB

--- pr_name/accounts/views.py
+++ pr_name/accounts/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import User
 from django.http import HttpResponse
 
 
 def accounts(request):
+    # username = request.body["username"]
+    # password = request.body["password"]
+    # # User.objects.create_user(username=username, password=password)
+
+    # user = User()
+    # user.username = username
+    # user.password = password
+    # user.save(commit=True)
     # return HttpResponse("hello")
     return render(request, "accounts.html", context={"name": "Basic"})
